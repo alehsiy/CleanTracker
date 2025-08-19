85c484a2ec9066f970e1a6614f14ddcc602ad192
Aleksey Zvorigin <a.zvorygin@vk.team>
2025-08-19 16:28:48 +0300

    Подготовил пустые экраны для работыЭ

---
+++ CleaningManager/CleaningManager/Scenes/ModalScreen/ModalScreenViewController.swift
@@ -0,0 +1,7 @@
+//
+//  ModalScreenViewController.swift
+//  CleaningManager
+//
+//  Created by Zvorygin Aleksey on 19.08.2025.
+//
+
---
+++ CleaningManager/CleaningManager/Scenes/Room/RoomViewController.swift
@@ -0,0 +1,7 @@
+//
+//  RoomViewController.swift
+//  CleaningManager
+//
+//  Created by Zvorygin Aleksey on 19.08.2025.
+//
+
